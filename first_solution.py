930adf6b7a2b6499699ccaa42bc7f029fdaeb7b3
Aman Yadav <103299347+Hare-krishnaa@users.noreply.github.com>
2023-06-18 21:47:21 +0530

    Create first_solution.py

---
+++ first_solution.py
@@ -0,0 +1,17 @@
+def mySqrt(x):
+    if x == 0:
+        return 0
+
+    left = 1
+    right = x
+
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * mid > x:
+            right = mid - 1
+        elif mid * mid < x:
+            left = mid + 1
+        else:
+            return mid
+
+    return right
